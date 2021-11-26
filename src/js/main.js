dcb8e7e9c86f0bba94243f1b3c23219f634044c5
armanaliani <61896630+armanaliani@users.noreply.github.com>
2021-11-25 19:18:51 -0500

    assesment submission

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,13 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                            '</h3>' +
+                            '<h2 class="reports_title">' +
+                                '<a href="{{url}}" target="_blank">{{title}} <span>{{file_size}} {{file_type}}</span></a>' +
+                            '</h2>' +
                         '{{/documents}}' +
                     '</footer>' +
                 '</article>' +
